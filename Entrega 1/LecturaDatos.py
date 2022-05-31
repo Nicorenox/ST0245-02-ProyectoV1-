7b6bf2c851da20cbbe20e09badf039007f6b66da
Nicorenox <100243743+Nicorenox@users.noreply.github.com>
2022-05-30 20:59:17 -0500

    Create LecturaDatos.py

---
+++ Entrega 1/LecturaDatos.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+map_acoso = pd.read_csv('https://raw.githubusercontent.com/mauriciotoro/ST0245-Eafit/master/proyecto/Datasets/calles_de_medellin_con_acoso.csv', sep=';')
+print(map_acoso)
+
+poligono = pd.read_csv('https://raw.githubusercontent.com/mauriciotoro/ST0245-Eafit/master/proyecto/Datasets/poligono_de_medellin.csv', sep=';')
+print(poligono)
